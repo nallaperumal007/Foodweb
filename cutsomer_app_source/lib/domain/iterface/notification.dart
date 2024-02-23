6859dba6d89b528640871d44464d019b402f5815
nallaperumal007 <nallaperumal342@gmail.com>
2024-02-23 09:37:46 +0530

    first

---
+++ cutsomer_app_source/lib/domain/iterface/notification.dart
@@ -0,0 +1,23 @@
+
+import 'package:riverpodtemp/domain/handlers/api_result.dart';
+import 'package:riverpodtemp/infrastructure/models/response/count_of_notifications_data.dart';
+import 'package:riverpodtemp/infrastructure/models/response/notification_response.dart';
+
+abstract class NotificationRepositoryFacade {
+
+  Future<ApiResult<NotificationResponse>> getNotifications({
+    int? page,
+    required String type,
+  });
+
+  Future<ApiResult<NotificationResponse>> getAllNotifications();
+
+  Future<ApiResult<dynamic>> readOne({
+    int? id,
+  });
+
+  Future<ApiResult<NotificationResponse>> readAll();
+
+
+  Future<ApiResult<CountNotificationModel>> getCount();
+}
